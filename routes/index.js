b1a8091f61bb558c40e486be4c8b64fe64e5c25a
SandrinePatin <44065719+SandrinePatin@users.noreply.github.com>
2020-03-19 16:42:15 +0100

    Add second age villes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Node App' });
 });
 
+router.get('/ville', function(req, res, next) {
+  res.render('ville', { ville: req.query.nom_ville });
+});
+
 module.exports = router;
